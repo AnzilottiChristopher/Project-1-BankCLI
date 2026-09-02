88803e6f5847e6d9127a501ef860879a20c36103
gr33npo1s0n <iyerchandramouli03@gmail.com>
2026-09-02 12:35:10 -0700

    Initial Project Structure

--- src/main/java/org/bankofcli/BankApplication.java
+++ src/main/java/org/bankofcli/BankApplication.java
@@ -1,8 +1,8 @@
-package org.example;
+package org.bankofcli;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class BankApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
