802aefe0f54fbb2590123b04caa24c0fe3f77d7c
Chris Anzilotti <anzilottichr@gmail.com>
2026-09-02 16:00:39 -0400

    Added basic balance function

--- src/main/java/org/bankofcli/api/Application.java
+++ src/main/java/org/bankofcli/api/Application.java
@@ -1,4 +1,10 @@
 package org.bankofcli.api;
 
 public class Application {
+    public String balance() {
+        //!TODO Read from database current balance
+        //!TODO then return balance
+
+        return "$100.00";
+    }
 }
