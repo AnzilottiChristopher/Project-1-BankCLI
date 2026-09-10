6e59563954bd54cce13438f8416bdf1f59c9ce9f
David Villarreal <your-email@example.com>
2026-09-10 15:30:17 -0500

    Update transfer functionality

--- src/main/java/org/bankofcli/service/impl/TransactionServiceImpl.java
+++ src/main/java/org/bankofcli/service/impl/TransactionServiceImpl.java
@@ -47,7 +47,7 @@ public class TransactionServiceImpl implements TransactionService {
                     "Source and destination accounts must be different.");
         }
 
-        transactionRepository.transfer(
+        transactions.transfer(
                 sourceAccountId,
                 destinationAccountId,
                 amount
